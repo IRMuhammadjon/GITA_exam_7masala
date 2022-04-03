673f53df4d9da6c9323aa89d12b01f571cb813af
IRMuhammadjon <102463648+IRMuhammadjon@users.noreply.github.com>
2022-04-03 07:15:36 +0500

    Добавьте файлы проекта.

---
+++ GITA_exam_7masala/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Sonni kiriting: ");
+var N= Convert.ToInt32(Console.ReadLine());
+int a, b,m=0;
+for (int i = 1; i <= N; i++)
+{
+    a = 0;
+    b=0;
+    for (int g = 1; g < i; g++)
+    {
+        if (i % g == 0)
+        {
+            a = a + g;
+        }
+    }
+    for (int f = 1; f < a; f++)
+    {
+
+        if (a % f == 0)
+        {
+            b = b + f;
+        }
+    }
+
+        if ( b == i && a!=b && b!=m)
+        {
+            Console.WriteLine(b.ToString() + "  " + a.ToString());
+        m = a;
+        }
+}
